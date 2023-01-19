37d2810f918d3a436cfc01ee8f1e50017c8083ed
shanishaju <115702546+shanishaju@users.noreply.github.com>
2023-01-19 14:20:38 +0530

    all c pgms

---
+++ sumfunction.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int myfunction(int x, int y)
+{
+int c;
+c=x+y;
+return(c);
+}
+int main()
+{
+int a;
+int b;
+printf("enter two numbers");
+scanf("%d%d",&a,&b);
+int result = myfunction(a,b);
+printf("result is = %d",result);
+return 0;
+}
